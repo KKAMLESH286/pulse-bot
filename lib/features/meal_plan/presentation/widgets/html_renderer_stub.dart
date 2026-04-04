ef4fe03c2e505aa13a42d8d97f0dbea5f8b2a258
Kamlesh Panwar <kkamlesh286@gmail.com>
2026-04-04 12:10:29 +0530

    feat(html_renderer): implement mobile HTML rendering with WebView support

--- lib/features/meal_plan/presentation/widgets/html_renderer_stub.dart
+++ lib/features/meal_plan/presentation/widgets/html_renderer_stub.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 Widget buildHtmlRenderer(String htmlContent, String viewType) {
-  return const Center(child: Text('HTML rendering is only supported on web'));
+  return _MobileHtmlRenderer(htmlContent: htmlContent);
 }
 
 String registerHtmlView(String htmlContent) {
-  return 'unsupported';
+  return 'mobile';
+}
+
+class _MobileHtmlRenderer extends StatefulWidget {
+  const _MobileHtmlRenderer({required this.htmlContent});
+  final String htmlContent;
+
+  @override
+  State<_MobileHtmlRenderer> createState() => _MobileHtmlRendererState();
+}
+
+class _MobileHtmlRendererState extends State<_MobileHtmlRenderer> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0xFF121212))
+      ..loadHtmlString(widget.htmlContent);
+  }
+
+  @override
+  void didUpdateWidget(_MobileHtmlRenderer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.htmlContent != widget.htmlContent) {
+      _controller.loadHtmlString(widget.htmlContent);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebViewWidget(controller: _controller);
+  }
 }
